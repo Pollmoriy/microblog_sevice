4db9738943b94982c52d39fcae678a66fecf463f
user <sevcovapolina4@gmail.com>
2026-05-27 13:05:13 +0300

    create schemas

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from typing import Literal
+from pydantic import BaseModel, Field
+
+class TweetCreate(BaseModel):
+    tweet_data: str = Field(min_length=1, max_length=800)
+    tweet_media_ids: list[int] | None = None
+
+class TweetResponse(BaseModel):
+    result: Literal[True] = True
+    tweet_id: int
+
+class MediaResponse(BaseModel):
+    result: Literal[True] = True
+    media_id: int
+
+class ErrorResponse(BaseModel):
+    result: Literal[False] = False
+    error_type: str
+    error_message: str
