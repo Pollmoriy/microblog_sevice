ea5c58ed603bfc3411b5056716479a291dd2ad3b
user <sevcovapolina4@gmail.com>
2026-05-30 18:35:20 +0300

    fix code

--- backend/database.py
+++ backend/database.py
@@ -7,6 +7,9 @@ from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set")
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = async_sessionmaker(
